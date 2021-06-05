38bb4c7b47ea26a7143f4229fa8815778dfd223d
Remy DUTHU <remy.duthu@gmail.com>
2021-06-05 13:14:37 +0200

    Add some benchmark tests

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ create several versions of the same structure without duplicating it. A use case
 could be when creating an API and not wanting to provide private information to
 end users.
 
+## Usage
+
 A simple example can be:
 
 ```go
@@ -55,4 +57,22 @@ b, _ := json.Marshal(user)
 fmt.Println(string(b))
 // Output:
 // {"email":"foo@bar.com"}
+```
+
+## Benchmarks
+
+Here are the results of a benchmark executed on my laptop. The results do not represent an absolute truth but will allow me not to regress during future updates.
+
+```
+goos: darwin
+goarch: amd64
+pkg: github.com/remydt/faces
+cpu: Intel(R) Core(TM) i7-8559U CPU @ 2.70GHz
+BenchmarkFaces-8                                  	 5030667	       231.3 ns/op	      40 B/op	       4 allocs/op
+BenchmarkFacesWithNestedStructure-8               	 2031132	       599.7 ns/op	     112 B/op	      10 allocs/op
+BenchmarkFacesWithArrayOfStructures-8             	 1724448	       686.8 ns/op	     120 B/op	      11 allocs/op
+BenchmarkFacesWithoutTags-8                       	534643616	         2.242 ns/op	       0 B/op	       0 allocs/op
+BenchmarkFacesWithSomethingElseThanAStructure-8   	129993452	         9.237 ns/op	       0 B/op	       0 allocs/op
+PASS
+coverage: 100.0% of statements
 ```
--- faces_test.go
+++ faces_test.go
@@ -11,6 +11,16 @@ type facesTestSuite struct {
 	suite.Suite
 }
 
+type admin struct {
+	Name string
+	Role string `faces:"private"`
+	user
+}
+
+type team struct {
+	Users []user
+}
+
 type user struct {
 	Email           string
 	Password        string `faces:"private"`
@@ -60,6 +70,19 @@ func (suite *facesTestSuite) TestShouldNotFilterWithoutTags() {
 	assert.Equal(suite.T(), expected, actual)
 }
 
+func (suite *facesTestSuite) TestShouldPanicWithoutAnAddress() {
+	var (
+		u = user{
+			Email:    "foo&bar.com",
+			Password: "123",
+		}
+	)
+
+	assert.Panics(suite.T(), func() {
+		Reveal(u, "public")
+	})
+}
+
 func (suite *facesTestSuite) TestShouldFilter() {
 	var (
 		actual = &user{
@@ -78,12 +101,23 @@ func (suite *facesTestSuite) TestShouldFilter() {
 }
 
 func (suite *facesTestSuite) TestShouldFilterMultipleTags() {
+	type identity struct {
+		FirstName string `faces:"private,public"`
+		LastName  string `faces:"confidential,private"`
+		BirthDate string `faces:"confidential"`
+	}
+
 	var (
-		actual = &user{
-			Email:    "foo&bar.com",
-			Password: "123",
+		actual = &identity{
+			FirstName: "Foo",
+			LastName:  "Bar",
+			BirthDate: "01/01/2000",
 		}
-		expected = actual
+		expected = &identity{
+			FirstName: "Foo",
+			LastName:  "Bar",
+			BirthDate: "",
+		}
 	)
 
 	Reveal(actual, "public", "private")
@@ -92,11 +126,6 @@ func (suite *facesTestSuite) TestShouldFilterMultipleTags() {
 }
 
 func (suite *facesTestSuite) TestShouldFilterNestedStructure() {
-	type admin struct {
-		Name string
-		Role string `faces:"private"`
-		user
-	}
 
 	var (
 		actual = &admin{
@@ -123,10 +152,6 @@ func (suite *facesTestSuite) TestShouldFilterNestedStructure() {
 }
 
 func (suite *facesTestSuite) TestShouldFilterArrayOfStructures() {
-	type team struct {
-		Users []user
-	}
-
 	var (
 		actual = &team{
 			Users: []user{
@@ -161,4 +186,71 @@ func (suite *facesTestSuite) TestShouldFilterArrayOfStructures() {
 
 func TestSuite(t *testing.T) {
 	suite.Run(t, new(facesTestSuite))
+}
+
+func BenchmarkFaces(b *testing.B) {
+	u := &user{
+		Email:    "foo&bar.com",
+		Password: "123",
+	}
+
+	for n := 0; n < b.N; n++ {
+		Reveal(u, "private")
+	}
+}
+
+func BenchmarkFacesWithNestedStructure(b *testing.B) {
+	a := &admin{
+		Name: "Foo",
+		Role: "Administrator",
+		user: user{
+			Email:    "foo&bar.com",
+			Password: "123",
+		},
+	}
+
+	for n := 0; n < b.N; n++ {
+		Reveal(a, "public")
+	}
+}
+
+func BenchmarkFacesWithArrayOfStructures(b *testing.B) {
+	t := &team{
+		Users: []user{
+			{
+				Email:    "foo&bar.com",
+				Password: "123",
+			},
+			{
+				Email:    "john&doe.com",
+				Password: "456",
+			},
+		},
+	}
+
+	for n := 0; n < b.N; n++ {
+		Reveal(t, "public")
+	}
+}
+
+func BenchmarkFacesWithoutTags(b *testing.B) {
+	u := &user{
+		Email:    "foo&bar.com",
+		Password: "123",
+	}
+
+	for n := 0; n < b.N; n++ {
+		Reveal(u)
+	}
+}
+
+func BenchmarkFacesWithSomethingElseThanAStructure(b *testing.B) {
+	m := &map[string]string{
+		"a": "b",
+	}
+
+	for n := 0; n < b.N; n++ {
+		// Will do nothing
+		Reveal(m, "public")
+	}
 }
